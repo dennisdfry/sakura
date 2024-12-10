0938aa54a9020297c0fb37f642059df6b1bdefba
Dennis Freyer <dennis.dfry@gmail.com>
2024-12-10 13:34:31 +0100

    update

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 
 @Component({
   selector: 'app-landingPage',
   standalone: true,
-  imports: [CommonModule],
-  template: '<p>landing-page works!</p>',
-  styles: ['h1 {color: red;}']
+  imports: [CommonModule, NavbarComponent],
+  template: `
+  <section>
+  <app-navbar></app-navbar>
+  <h1 class="fontRaleway">SAKURA RAMEN</h1>
+  <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
+  </section>`,
+  styleUrls:['./landing-page.component.scss']
 })
 export class LandingPageComponent {
 
